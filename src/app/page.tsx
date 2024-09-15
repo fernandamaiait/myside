e5338d7b8358774f3e3ab9de672bde05e051edab
Fernanda Maia <47039574+fernandamaiait@users.noreply.github.com>
2024-09-15 12:03:40 -0300

    feat: add main page (products list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,43 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import ProductGrid from "./components/product-grid/product-grid";
+import Loading from "./loading";
 
-export default function Home() {
+const fetchData = async () => {
+  try {
+    const [productsResponse, categoriesResponse] = await Promise.all([
+      fetch("https://fakestoreapi.in/api/products"),
+      fetch("https://fakestoreapi.in/api/products/category"),
+    ]);
+
+    if (!productsResponse.ok || !categoriesResponse.ok) {
+      throw new Error("Erro ao buscar dados");
+    }
+
+    const products = await productsResponse.json();
+    const categories = await categoriesResponse.json();
+
+    return { products: products.products, categories: categories.categories };
+  } catch (error) {
+    alert("Erro ao buscar dados: " + error);
+    return { products: [], categories: [] };
+  }
+};
+
+const Home = async () => {
+  const data = await fetchData();
+
+  if (!data.products.length) {
+    return <Loading />;
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <ProductGrid products={data.products} />
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
-}
+};
+
+export default Home;
