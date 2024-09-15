256c1c3862830ede7ae2086249b3a3db1e9755ce
Fernanda Maia <47039574+fernandamaiait@users.noreply.github.com>
2024-09-15 12:04:23 -0300

    feat: add Product type

---
+++ src/app/general-types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  id: string;
+  image: string;
+  title: string;
+  price: number;
+  description: string;
+  brand: string;
+  model: string;
+  color: string;
+  category: string;
+  discount: number;
+};
